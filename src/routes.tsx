e04c59fed1caebad1fb7bdc7a654ae7c2e62aea5
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-10-21 21:36:30 -0300

    adicionado nova rota

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,14 +4,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
 
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
