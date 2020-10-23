75f2541b65baa193c4ddb5dbf5ca6e5e63c6dde1
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-10-22 21:36:29 -0300

    pagina de perfil do orfanato pronta

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,72 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Image, Text, ScrollView} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+
+import MapView, {Marker} from 'react-native-maps';
+
+import MarkerIcon from '../../assets/icons/Local.png';
+
+import styles from './styles';
 
 function Profile() {
-  return <Text>asdf</Text>;
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              'https://f.i.uol.com.br/fotografia/2018/07/26/15326507755b5a65177921a_1532650775_3x2_rt.jpg',
+          }}
+        />
+        <View style={styles.content}>
+          <View>
+            <Text style={styles.title}>Orf.Esperança</Text>
+            <Text style={styles.about}>
+              Presta assistência a crianças de 06 a 15 anos que se encontre em
+              situação de risco e/ou vulnerabilidade social.
+            </Text>
+          </View>
+          <View style={styles.orphanageLocation}>
+            <MapView
+              style={styles.map}
+              initialRegion={{
+                latitude: -3.5552224,
+                longitude: -41.1272681,
+                latitudeDelta: 0.008,
+                longitudeDelta: 0.008,
+              }}>
+              <Marker
+                coordinate={{latitude: -3.5552224, longitude: -41.1272681}}
+                icon={MarkerIcon}
+              />
+            </MapView>
+            <Text style={styles.gmButton}>Ver rotas no Google Maps</Text>
+          </View>
+          <View style={styles.instructions}>
+            <Text style={styles.instructionsTitle}>
+              Instruções para a visita
+            </Text>
+            <Text style={styles.about}>
+              Venha como se sentir a vontade e traga muito amor e paciência para
+              dar.
+            </Text>
+            <View style={styles.instructionsBoxes}>
+              <View style={styles.box}>
+                <Icon name="clock" size={40} color="#2AB5D1" />
+                <Text style={styles.boxText}>Segunda à Sexta 8h às 18h</Text>
+              </View>
+              <View style={[styles.box, styles.openWeekend]}>
+                <Icon name="alert-circle" size={40} color="#2AB5D1" />
+                <Text style={styles.boxText}>Atendemos fim de semana</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 export default Profile;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,94 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CEDEE5',
+  },
+
+  image: {
+    width: '100%',
+    height: 260,
+  },
+
+  content: {
+    padding: 20,
+  },
+
+  title: {
+    color: '#4D6F80',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+
+  about: {
+    color: '#5C8599',
+    fontSize: 15,
+    fontWeight: '600',
+    marginTop: 20,
+    lineHeight: 25,
+  },
+
+  orphanageLocation: {
+    backgroundColor: '#E6F7FB',
+    marginTop: 20,
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+
+  map: {
+    height: 196,
+    width: '100%',
+  },
+
+  gmButton: {
+    color: '#0089A5',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    padding: 20,
+  },
+
+  instructions: {
+    marginTop: 39,
+  },
+
+  instructionsTitle: {
+    fontSize: 24,
+    color: '#4D6F80',
+    fontWeight: '700',
+  },
+
+  instructionsBoxes: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    marginTop: 24,
+  },
+
+  box: {
+    backgroundColor: '#E6F7FB',
+    width: 159,
+    height: 160,
+    padding: 20,
+    borderRadius: 20,
+    borderStyle: 'solid',
+    borderColor: '#B3DAE2',
+    borderWidth: 1,
+  },
+
+  boxText: {
+    width: 125,
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#5C8599',
+    lineHeight: 25,
+    marginTop: 16,
+  },
+
+  openWeekend: {
+    backgroundColor: '#EDFFF6',
+  },
+});
+
+export default styles;
