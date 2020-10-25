3713aae8927e094a83760107cc991567a6d7d237
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-10-24 23:56:41 -0300

    upload de imagem para o app feito

--- src/pages/RegisterForm/index.tsx
+++ src/pages/RegisterForm/index.tsx
@@ -1,12 +1,40 @@
 import React, {useState} from 'react';
-import {View, Text, ScrollView, TouchableOpacity, Switch} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Switch,
+  Image,
+} from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
+
+import {BorderlessButton} from 'react-native-gesture-handler';
+
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 
 import styles from './styles';
 
+interface ImageProps {
+  path: string;
+  name: string;
+}
+
 function RegisterForm() {
   const [eneabled, setEneabled] = useState(false);
+
+  const [images, setImages] = useState<Array<ImageProps>>([]);
+
+  function handleFile() {
+    ImagePicker.launchImageLibrary({}, (response) => {
+      if (response.uri) {
+        setImages([...images, {path: response.uri!, name: response.fileName!}]);
+      }
+    });
+  }
 
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
@@ -15,7 +43,27 @@ function RegisterForm() {
         <Input label="Nome" />
         <Input label="Sobre" multiline textArea={true} />
         <Input label="Número de Whatsapp" />
-        <Input label="Fotos" />
+        <View style={styles.fileContainer}>
+          <Text style={styles.label}>Fotos</Text>
+          {images.map((image) => (
+            <View key={image.name} style={styles.imageContainer}>
+              <View style={styles.profileContainer}>
+                <Image source={{uri: image.path}} style={styles.image} />
+                <Text style={styles.imageName} numberOfLines={1}>
+                  {image.name}
+                </Text>
+              </View>
+              <BorderlessButton>
+                <Icon name="x" size={24} color="#FF669D" />
+              </BorderlessButton>
+            </View>
+          ))}
+          <TouchableOpacity
+            style={[styles.button, styles.buttonFile]}
+            onPress={handleFile}>
+            <Icon name="plus" size={30} color="#15B6D6" />
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={styles.visitationContainer}>
         <Text style={styles.title}>Visitação</Text>
--- src/pages/RegisterForm/styles.ts
+++ src/pages/RegisterForm/styles.ts
@@ -42,10 +42,54 @@ const styles = StyleSheet.create({
     marginBottom: 40,
   },
 
+  buttonFile: {
+    backgroundColor: '#FFF',
+    borderColor: '#15B6D6',
+    borderWidth: 1,
+    borderStyle: 'dotted',
+    marginTop: 10,
+    marginBottom: 0,
+  },
+
+  fileContainer: {
+    marginTop: 20,
+  },
+
   buttonText: {
     color: '#FFF',
     fontSize: 15,
     fontWeight: 'bold',
+  },
+
+  imageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#EDFFF6',
+    borderRadius: 20,
+    padding: 5,
+    borderColor: '#A1E9C5',
+    borderWidth: 1,
+    marginTop: 5,
+  },
+
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 16,
+  },
+
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+  },
+
+  imageName: {
+    color: '#37C77F',
+    fontSize: 15,
+    marginLeft: 10,
+    flex: 1,
   },
 });
 
